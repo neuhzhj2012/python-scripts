bbd148a4b31159e87888d582719a94838404cee1
hzhj2012 <huangzhongjie@autohome.com.cn>
2019-07-19 09:57:36 +0800

    输出dict文件

--- Func_reptile_google_img.py
+++ Func_reptile_google_img.py
@@ -3,17 +3,22 @@
 py2无法处理中文，可换成py3
 '''
 import sys
+import numpy as np
 print ('encode: {}, path: {}'.format(sys.getdefaultencoding(), sys.path ))
 
 from google_images_download import google_images_download
 response = google_images_download.googleimagesdownload()
 
 # keywords = '中控,汽车, 汽车尾部 排气管,前雾灯,尾灯,仪表盘,油箱口'
-keywords = "中网, 汽车"
+keywords = "风景,旅行风景"
 
-arguments = {"keywords":keywords,"limit":110,"print_urls":True,
-             "format":"jpg", "size":">400*300",
+arguments = {"keywords":keywords,"limit":2,"print_urls":True,
+             "format":"jpg", "size":">400*300","no_download":True,
              "chromedriver":"C:\\chromedriver_win32\\chromedriver.exe"}
 print (arguments)
 rst = response.download(arguments)
-print ("num: ",len(rst[0]['中网']))
+print ("num: {}, val: {}".format(len(rst[0]['风景']), rst[0]['风景']))
+
+np.save('keywords.npy', rst[0]) #保存为
+
+# kv=np.load('keywords.npy').item() #数据加载
