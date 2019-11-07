d01ffeab6a60adc42b3c51777ab1c2efff19d333
hzhj2012 <huangzhongjie@autohome.com.cn>
2019-11-07 20:09:45 +0800

    增加参数测试

--- Func_reptile_google_img.py
+++ Func_reptile_google_img.py
@@ -10,15 +10,41 @@ from google_images_download import google_images_download
 response = google_images_download.googleimagesdownload()
 
 # keywords = '中控,汽车, 汽车尾部 排气管,前雾灯,尾灯,仪表盘,油箱口'
-keywords = "风景,旅行风景"
+# keywords = "骑行 摩托车, 摩托车 旅游, 摩托车 公路, 摩托车 运动, \
+#            乐趣 摩托车,摩托车 北极光,摩托车 宗申,摩托车 重型机车,重型机车,\
+#            重型机车 运动"
 
-arguments = {"keywords":keywords,"limit":2,"print_urls":True,
+keywords = "自行车, 山地自行车,山地车, 骑行, 骑山地自行车,自行车 旅游, 自行车 运动, \
+           山地车 乐趣, 自行车 乐趣"
+
+
+
+arguments = {"keywords":keywords,"limit":5000,"print_urls":True,
              "format":"jpg", "size":">400*300","no_download":True,
              "chromedriver":"C:\\chromedriver_win32\\chromedriver.exe"}
 print (arguments)
-rst = response.download(arguments)
-print ("num: {}, val: {}".format(len(rst[0]['风景']), rst[0]['风景']))
+rst_jpg = response.download(arguments)
+arguments = {"keywords":keywords,"limit":5000,"print_urls":True,
+             "format":"png", "size":">400*300","no_download":True,
+             "chromedriver":"C:\\chromedriver_win32\\chromedriver.exe"}
+rst_png = response.download(arguments)
 
-np.save('keywords.npy', rst[0]) #保存为
+# rst = {keywords:rst_jpg[0][keywords] + rst_png[0][keywords] }
+
+urls = []
+for k,v in rst_jpg[0].items():
+    urls +=v
+for k,v in rst_png[0].items():
+    urls +=v
+
+print ('num: {}'.format(len(urls)))
+# rst = {keywords:rst_jpg[0][keywords] + rst_png[0][keywords] }
+rst = {keywords:urls }
+
+
+# print ("num: {}, val: {}".format(len(rst[0]['风景']), rst[0]['风景']))
+
+# np.save('keywords.npy', rst[0]) #保存为
+np.save('bike.npy', rst) #保存为
 
 # kv=np.load('keywords.npy').item() #数据加载
