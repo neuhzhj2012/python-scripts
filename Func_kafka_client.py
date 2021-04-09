5896266d65317659d45d2eb6caf439f60079c705
huangzhongjie <huangzhongjie@soulapp.cn>
2021-04-09 17:26:06 +0800

    去除topic字段

--- Func_kafka_client.py
+++ Func_kafka_client.py
@@ -2,9 +2,11 @@ from kafka import KafkaConsumer
 
 if __name__ == '__main__':
 
-    consumer = KafkaConsumer(topic='tmp',
+    consumer = KafkaConsumer('post-info-cache',
                              bootstrap_servers=[
-                                 'offline-kafka-1.****.cn:9092',
+                                 'log-kafka-1.soulapp.cn:9092',
+                                 'log-kafka-2.soulapp.cn:9092',
+                                 'log-kafka-3.soulapp.cn:9092'
                              ],
                              # auto_offset_reset='',
                              auto_offset_reset='latest',  # 消费kafka中最近的数据，如果设置为earliest则消费最早的数据，不管这些数据是否消费
@@ -19,6 +21,7 @@ if __name__ == '__main__':
     for msg in consumer:
         # print (msg.value)
         msg = eval(msg.value)
+        print (msg.keys())
         score = msg.get("score")
         img_url = msg.get('imageUrl')
         print (str(score)+"\t"+ img_url)
