f85b54ccedd1bb24faf7b96fdaefc8acf59b7fc0
huangzhongjie <huangzhongjie@soulapp.cn>
2021-04-09 16:49:37 +0800

    kafka客户端

---
+++ Func_kafka_client.py
@@ -0,0 +1,25 @@
+from kafka import KafkaConsumer
+
+if __name__ == '__main__':
+
+    consumer = KafkaConsumer(topic='tmp',
+                             bootstrap_servers=[
+                                 'offline-kafka-1.****.cn:9092',
+                             ],
+                             # auto_offset_reset='',
+                             auto_offset_reset='latest',  # 消费kafka中最近的数据，如果设置为earliest则消费最早的数据，不管这些数据是否消费
+                             # auto_offset_reset='earliest',  # 消费kafka中最近的数据，如果设置为earliest则消费最早的数据，不管这些数据是否消费
+                             enable_auto_commit=True,  # 自动提交消费者的offset
+                             auto_commit_interval_ms=3000,  ## 自动提交消费者offset的时间间隔
+                             group_id='test',
+                             consumer_timeout_ms=10000,  # 如果10秒内kafka中没有可供消费的数据，自动退出
+                             client_id='consumer-python3'
+                             )
+
+    for msg in consumer:
+        # print (msg.value)
+        msg = eval(msg.value)
+        score = msg.get("score")
+        img_url = msg.get('imageUrl')
+        print (str(score)+"\t"+ img_url)
+
